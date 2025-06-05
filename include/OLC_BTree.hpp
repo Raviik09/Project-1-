be1d03435c2e50cfac8d4b1fbe2b5e0e4cc48ba2
Raviik09 <118880797+Raviik09@users.noreply.github.com>
2025-06-05 23:58:19 +0200

    Update OLC_BTree.hpp

--- include/OLC_BTree.hpp
+++ include/OLC_BTree.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
 #include "OptLatch.hpp"
+#include <atomic>
+#include <mutex>
+
 // -------------------------------------------------------------------------------------
 // BTREE NODES
-// -------------------------------------------------------------------------------------
-// Methods could be
-// - lowerBound
-// - split
-// - insert
-// for both node types, i.e., inner and leaf
-// The Nodes are internal to your B-Tree logic, therefore feel free to change them
-// the given methods here are just a blueprint and can be changed as you see fit
 // -------------------------------------------------------------------------------------
 
 using Key = uint64_t;
@@ -21,7 +16,7 @@ static constexpr uint64_t pageSize=4*1024; // DO NOT CHANGE 4KB size nodes
 
 struct NodeBase : public OptLatch{
    NodeType type;
-   uint16_t count;
+   uint16_t count; 
 };
 
 struct BTreeLeafBase : public NodeBase {
@@ -32,68 +27,62 @@ struct BTreeInnerBase : public NodeBase {
    static const NodeType typeMarker=NodeType::BTreeInner;
 };
 
+// -------------------------------------------------------------------------------------
 struct BTreeLeaf : public BTreeLeafBase {
-   // -------------------------------------------------------------------------------------
-   struct Entry {
-      Key k;
-      Payload p;
-   };
-   // -------------------------------------------------------------------------------------
-   static constexpr uint64_t maxEntries=(pageSize-sizeof(NodeBase))/(sizeof(Key)+sizeof(Payload));
-   Key keys[maxEntries];
-   Payload payloads[maxEntries];
-   // -------------------------------------------------------------------------------------
-   BTreeLeaf() {
-      count=0;
-      type=typeMarker;
-   }
-   // -------------------------------------------------------------------------------------
-   bool isFull() { return count==maxEntries; };
-   unsigned lowerBound(Key k); // Implement
-   void insert(Key k,Payload p); // Implement
-   BTreeLeaf* split(Key& sep); // Implement  
-};
+  
+   static constexpr uint64_t ENTRIES_CAPACITY = (pageSize - sizeof(NodeBase)) / (sizeof(Key) + sizeof(Payload));
+   
+   Key keys[ENTRIES_CAPACITY + 1]; 
+   Payload payloads[ENTRIES_CAPACITY + 1]; 
 
+   BTreeLeaf() {
+      type = typeMarker;
+      count = 0;
+   }
+
+   bool isFull() { return count == ENTRIES_CAPACITY; }
+   unsigned lowerBound(Key k);
+   void insert(Key k, Payload p);
+   BTreeLeaf* split(Key& sep);
+};
 // -------------------------------------------------------------------------------------
 struct BTreeInner : public BTreeInnerBase {
-   static const uint64_t maxEntries=(pageSize-sizeof(NodeBase))/(sizeof(Key)+sizeof(NodeBase*));
-   NodeBase* children[maxEntries];
-   Key keys[maxEntries];
-   // -------------------------------------------------------------------------------------
-   BTreeInner() {
-      count=0;
-      type=typeMarker;
-   }
-   // -------------------------------------------------------------------------------------
-   bool isFull() { return count==(maxEntries-1); };
-   unsigned lowerBound(Key k); // implement 
-   BTreeInner* split(Key& sep); // implement
-   void insert(Key k,NodeBase* child); // implement
+     static constexpr uint64_t KEYS_CAPACITY = (pageSize - sizeof(NodeBase) - sizeof(NodeBase*)) / (sizeof(Key) + sizeof(NodeBase*));
 
+   Key keys[KEYS_CAPACITY + 1]; 
+   NodeBase* children[KEYS_CAPACITY + 1 + 1]; 
+
+   BTreeInner() {
+      type = typeMarker;
+      count = 0;
+   }
+
+   bool isFull() { return count == KEYS_CAPACITY; }
+   unsigned lowerBound(Key k); 
+   unsigned getChildIdxForTraversal(Key k) const; 
+   BTreeInner* split(Key& sep);
+   void insert(Key k, NodeBase* child);
 };
-// -------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------
 // BTREE 
 // -------------------------------------------------------------------------------------
-// Implement upsert and lookup, do not change the function signature as we test against this
-// interface.
-// You do not need to store duplicate keys, we just update them in the upsert method.
-// All inputs key, and values, are uint64_t.
-
 class OLC_BTree {
-  private:
+public:
    std::atomic<NodeBase*> root;
    std::atomic<uint64_t> height;
-   // feel free to add variables      
-   void makeRoot(Key k,NodeBase* leftChild,NodeBase* rightChild); // implement
-   
-   public:
+   std::mutex treeMutex; 
+
+public:
    OLC_BTree() {
       root = new BTreeLeaf();
+      height = 1;
    }
-   uint64_t getHeight(){return height;}
-   void upsert(Key k, Payload v); // implement upsert, i.e. insert or update if key exists
-   bool lookup(Key k, Payload& result); // implement lookup
-   
-};
 
+   ~OLC_BTree() {
+     
+   }
+
+   bool lookup(Key k, Payload& result);
+   void upsert(Key k, Payload v);
+   bool makeRoot(Key k, NodeBase* leftChild, NodeBase* rightChild);
+};
